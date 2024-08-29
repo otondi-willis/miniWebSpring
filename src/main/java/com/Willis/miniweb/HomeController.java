1e0d71189044ba4ded009fe79459e576cda4504a
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-29 13:00:43 +0300

    Initial Commit

---
+++ src/main/java/com/Willis/miniweb/HomeController.java
@@ -0,0 +1,10 @@
+package com.Willis.miniweb;
+
+import org.springframework.stereotype.Controller;
+
+
+public class HomeController {
+    public String greet(){
+        return "Welcome to Web Project";
+    }
+}
---
+++ src/main/java/com/Willis/miniweb/MiniwebApplication.java
@@ -0,0 +1,13 @@
+package com.Willis.miniweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniwebApplication.class, args);
+	}
+
+}
