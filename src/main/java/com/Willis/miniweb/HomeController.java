71672919e8d08eddb5128e329e4fe6e4452c90da
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-29 13:11:46 +0300

    use of RestController since its only returning data not page

--- src/main/java/com/Willis/miniweb/HomeController.java
+++ src/main/java/com/Willis/miniweb/HomeController.java
@@ -3,11 +3,11 @@ package com.Willis.miniweb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
     @RequestMapping("/")
-    @ResponseBody
     public String greet(){
         return "Welcome to Web Project";
     }
