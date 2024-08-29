5046b734a3af729f2f31a1385c8def696b6c61fb
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-29 13:01:24 +0300

    Controller class

--- src/main/java/com/Willis/miniweb/HomeController.java
+++ src/main/java/com/Willis/miniweb/HomeController.java
@@ -2,7 +2,7 @@ package com.Willis.miniweb;
 
 import org.springframework.stereotype.Controller;
 
-
+@Controller
 public class HomeController {
     public String greet(){
         return "Welcome to Web Project";
