e3ab6ac8e42fd3454e9e7a44bfcb2d578a09799f
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-29 13:05:54 +0300

    Request Mapping

--- src/main/java/com/Willis/miniweb/HomeController.java
+++ src/main/java/com/Willis/miniweb/HomeController.java
@@ -1,9 +1,11 @@
 package com.Willis.miniweb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @RequestMapping("/")
     public String greet(){
         return "Welcome to Web Project";
     }
