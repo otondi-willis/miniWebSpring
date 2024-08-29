c304a426c2304f5f13a53938e835edd868ca8113
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-29 13:09:08 +0300

    Returns response body

--- src/main/java/com/Willis/miniweb/HomeController.java
+++ src/main/java/com/Willis/miniweb/HomeController.java
@@ -2,10 +2,12 @@ package com.Willis.miniweb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
+    @ResponseBody
     public String greet(){
         return "Welcome to Web Project";
     }
