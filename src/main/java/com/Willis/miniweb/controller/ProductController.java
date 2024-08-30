d0ceede0fb4a3bbd5882eb8fc36d550a11cbc199
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-30 05:57:38 +0300

    getting prod by prod id

--- src/main/java/com/Willis/miniweb/controller/ProductController.java
+++ src/main/java/com/Willis/miniweb/controller/ProductController.java
@@ -3,7 +3,9 @@ package com.Willis.miniweb.controller;
 import com.Willis.miniweb.model.Product;
 import com.Willis.miniweb.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,5 +18,10 @@ public class ProductController {
 
     public List<Product> getProducts(){
         return service.getProducts();
+    }
+    @RequestMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){
+        return service.getProductById(prodId);
+
     }
 }
--- src/main/java/com/Willis/miniweb/service/ProductService.java
+++ src/main/java/com/Willis/miniweb/service/ProductService.java
@@ -17,4 +17,10 @@ public class ProductService {
     public List<Product> getProducts(){
         return products;
     }
+
+    public Product getProductById(int prodId) {
+        return products.stream()
+                .filter(p->p.getProdId()==prodId)
+                .findFirst().get();
+    }
 }
