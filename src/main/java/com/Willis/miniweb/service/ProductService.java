0dd74a7dd3622c4c13b3136efbeb371a58119787
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-30 05:22:08 +0300

    Additional product added

--- src/main/java/com/Willis/miniweb/service/ProductService.java
+++ src/main/java/com/Willis/miniweb/service/ProductService.java
@@ -1,14 +1,20 @@
 package com.Willis.miniweb.service;
 
 import com.Willis.miniweb.model.Product;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+@Service
 
 public class ProductService {
     List<Product> products = Arrays.asList(new Product(101,"IphoneX",40000),
-            new Product(102,"Mulika",4000));
+            new Product(102,"Mulika",4000),
+            new Product(103,"itel",400));
+
     public List<Product> getProducts(){
-        return null;
+        return products;
     }
 }
