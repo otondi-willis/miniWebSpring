5c73477d3d67765094e442b7346ed42dcef0a860
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-30 05:33:53 +0300

    request response mapped

--- src/main/java/com/Willis/miniweb/controller/ProductController.java
+++ src/main/java/com/Willis/miniweb/controller/ProductController.java
@@ -3,12 +3,16 @@ package com.Willis.miniweb.controller;
 import com.Willis.miniweb.model.Product;
 import com.Willis.miniweb.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+@RestController
 
 public class ProductController {
     @Autowired
     ProductService service;
+    @RequestMapping("/products")
 
     public List<Product> getProducts(){
         return service.getProducts();
