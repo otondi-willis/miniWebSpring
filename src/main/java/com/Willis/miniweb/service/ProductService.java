8378db73e2440f64c3ba669f5d16e856f92212a6
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-30 05:06:02 +0300

    sample product list provided

--- src/main/java/com/Willis/miniweb/service/ProductService.java
+++ src/main/java/com/Willis/miniweb/service/ProductService.java
@@ -6,7 +6,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ProductService {
-    List<Product> products = Arrays.asList(new Product());
+    List<Product> products = Arrays.asList(new Product(101,"IphoneX",40000),
+            new Product(102,"Mulika",4000));
     public List<Product> getProducts(){
         return null;
     }
