d21db1d80085847eef573f7770dac76bc5c201d5
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-30 08:51:09 +0300

    method to add product

--- src/main/java/com/Willis/miniweb/service/ProductService.java
+++ src/main/java/com/Willis/miniweb/service/ProductService.java
@@ -5,14 +5,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 @Service
 
 public class ProductService {
-    List<Product> products = Arrays.asList(new Product(101,"IphoneX",40000),
+    List<Product> products = new ArrayList<>(Arrays.asList(new Product(101,"IphoneX",40000),
             new Product(102,"Mulika",4000),
-            new Product(103,"itel",400));
+            new Product(103,"itel",400)));
 
     public List<Product> getProducts(){
         return products;
@@ -22,5 +23,8 @@ public class ProductService {
         return products.stream()
                 .filter(p->p.getProdId()==prodId)
                 .findFirst().get();
+    }
+    public void addProduct(Product prod){
+        products.add(prod);
     }
 }
