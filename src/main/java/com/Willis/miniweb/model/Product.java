42c8d18670d970e6026a93cfba98dd4eb65b4e80
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-30 05:21:49 +0300

    lombok misbehaving, manual setter, getter and constructor creation

--- src/main/java/com/Willis/miniweb/model/Product.java
+++ src/main/java/com/Willis/miniweb/model/Product.java
@@ -2,12 +2,41 @@ package com.Willis.miniweb.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
-@Data
-@AllArgsConstructor
-
+@Component
 public class Product {
     private int prodId;
     private String prodName;
     private int price;
+
+    public Product(int prodId, String prodName, int price) {
+        this.prodId = prodId;
+        this.prodName = prodName;
+        this.price = price;
+    }
+
+    public int getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(int prodId) {
+        this.prodId = prodId;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
