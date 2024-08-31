420f2a6702a2d24e32df9f34efdbc6f48109b402
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-31 09:20:28 +0300

    @DeleteMapping

--- src/main/java/com/Willis/miniweb/service/ProductService.java
+++ src/main/java/com/Willis/miniweb/service/ProductService.java
@@ -39,5 +39,12 @@ public class ProductService {
     }
 
     public void deleteProduct(int prodId) {
+        int index=0;
+        for(int i=0;i<products.size();i++){
+            if(products.get(i).getProdId()==prodId) {
+                index = i;
+
+            }
+        products.remove(index);}
     }
 }
