a694bf29761816c65d8623dcc13f5fe0607aac60
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-01 13:54:54 +0300

    created ProductRepo interface that extends JpaRepository

--- src/main/java/com/Willis/miniweb/repository/ProductRepo.java
+++ src/main/java/com/Willis/miniweb/repository/ProductRepo.java
@@ -1,4 +1,9 @@
 package com.Willis.miniweb.repository;
 
-public interface ProductRepo {
+import com.Willis.miniweb.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
 }
